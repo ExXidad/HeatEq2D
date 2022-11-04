be24cc4538d0f9d588d8d579270bf6d1832115f9
xidad <kolesnikov.iv@phystech.edu>
2022-11-04 22:16:23 +0300

    2

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@
 #include "BoundingRect.h"
 #include "Solver.h"
 
+constexpr double h = 1e-2;
 constexpr double xmin = 0., xmax = 2.;
 constexpr double ymin = 0., ymax = 1.;
-constexpr double T0 = 1.;
+constexpr double T0 = 100.;
 BoundingRect boundingRect(xmin, xmax, ymin, ymax);
 BoundingRect lambdaRect(xmin, xmin + 0.5, ymax - 0.5, ymax);
 BoundingRect bottomRect(xmin - 0.5, xmax + 0.5, -0.5 + ymin, ymin);
@@ -20,33 +21,51 @@ bool domainFunction(const double &x, const double &y)
 
 bool constTempFCond(const double &x, const double &y)
 {
-	return !boundingRect.contains(x, y);
-//	return (bottomRect.contains(x, y)
-//			||
-//			topRect.contains(x, y));
+//	return !boundingRect.contains(x, y);
+//			std::pow(x - 1.5, 2.) + std::pow(y - 0.5, 2.) <= std::pow(0.25, 2.)
+//			&&
+//			std::pow(x - 1.5, 2.) + std::pow(y - 0.5, 2.) >= std::pow(0.25 - 5 * h, 2.)
+	return (
+			bottomRect.contains(x, y)
+			||
+			topRect.contains(x, y)
+			||
+			std::pow(x - 1.5, 2.) + std::pow(y - 0.5, 2.) <= std::pow(0.25, 2.)
+			&&
+			std::pow(x - 1.5, 2.) + std::pow(y - 0.5, 2.) >= std::pow(0.25 - 5 * h, 2.)
+	);
 }
 
 double lambdaF(const double &x, const double &y)
 {
-	if (lambdaRect.contains(x, y))
-		return 1e20;
+	if (std::pow(x - 1.5, 2.) + std::pow(y - 0.5, 2.) <= std::pow(0.25-5*h, 2.))
+		return 1e15;
 	else
 		return 1.;
 }
 
-double sourceF(const double &x, const double &y, const double& T)
+double sourceF(const double &x, const double &y, const double &T)
 {
 //	return BoundingRect(0.5, 1.5, 0.25, 0.75).contains(x, y);
-	return 0.;
+	double M = 1e20;
+	if (std::pow(x - 1.5, 2.) + std::pow(y - 0.5, 2.) <= std::pow(0.25 - 5 * h, 2.))
+		return M * (T0 - T);
+	else
+		return 0.;
 }
 
 double tempF(const double &x, const double &y)
 {
 //	return y < (ymin + ymax) * 0.5 ? 50 : 150;
-	if (bottomRect.contains(x, y))
-		return 50.;
-	else if (topRect.contains(x, y))
-		return 150.;
+//	if (constTempFCond(x, y)) return T0;
+	if (bottomRect.contains(x, y)) return 50.;
+	else if (topRect.contains(x, y)) return 150;
+	else if (
+			std::pow(x - 1.5, 2.) + std::pow(y - 0.5, 2.) <= std::pow(0.25, 2.)
+			&&
+			std::pow(x - 1.5, 2.) + std::pow(y - 0.5, 2.) >= std::pow(0.25 - 5 * h, 2.)
+			)
+		return T0;
 }
 
 
@@ -59,8 +78,8 @@ int main(int argc, char **argv)
 	domain.addDomainFunction(domainFunction);
 
 	// Initialize solver
-	Solver solver(boundingRect, domain, 1e-2,
-				  tempF, constTempFCond, lambdaF, sourceF, 100.);
+	Solver solver(boundingRect, domain, h,
+				  tempF, constTempFCond, lambdaF, sourceF, T0);
 
 	std::cout << "Run details:" << std::endl;
 	std::cout << "h: " << solver.getH() << std::endl;
@@ -68,10 +87,10 @@ int main(int argc, char **argv)
 	std::cout << "Amount of cells: " << solver.getNy() * solver.getNx() << std::endl;
 
 	// Start solving
-	solver.solve(1e-3);
+	solver.solve(1e-4);
 
 	std::fstream file;
-	file.open("temp1.txt", std::ios::out);
+	file.open("temp2.txt", std::ios::out);
 	solver.exportTemp(file, false);
 	file.close();
 
